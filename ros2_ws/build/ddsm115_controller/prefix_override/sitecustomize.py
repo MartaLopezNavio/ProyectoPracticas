3087453a9058702bdf03cb63eab33e192e3b0c25
Marta <al417883@uji.es>
2026-04-14 10:30:19 +0200

    Scripts uploaded

--- ros2_ws/build/ddsm115_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/marta/Documents/4º/practicas/ros2_ws/install/ddsm115_controller'
--- ros2_ws/build/pose_landmarks_pkg/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/marta/Documents/4º/practicas/ros2_ws/install/pose_landmarks_pkg'
